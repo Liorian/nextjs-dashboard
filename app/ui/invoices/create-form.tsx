290ae06764e5d55c020965ce0aa29f02194dddee
QiuLin-hao <210554114@qq.com>
2024-10-26 11:56:51 +0800

    更新 `updateInvoice` 函数及相关导入
    重构了 `updateInvoice` 函数，添加了验证和错误处理，并更新了 `create-form.tsx` 和 `edit-form.tsx` 中的导入以反映这些更改。此外，在 `edit-form.tsx` 中集成了 `useActionState` 以实现更好的状态管理。

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import {CustomerField} from '@/app/lib/definitions';
+import {CustomerField, InvoiceForm} from '@/app/lib/definitions';
 import Link from 'next/link';
 import {
     CheckIcon,
@@ -9,7 +9,7 @@ import {
     UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import {Button} from '@/app/ui/button';
-import {createInvoice, State} from '@/app/lib/actions';
+import {createInvoice, State, updateInvoice} from '@/app/lib/actions';
 import {useActionState} from 'react';
 
 export default function Form({customers}: { customers: CustomerField[] }) {
