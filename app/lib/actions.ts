dba1959d2dddd7ec340cb8014ed7e9035c8871d0
QiuLin-hao <210554114@qq.com>
2024-10-26 11:49:54 +0800

    添加 ESLint 配置并改进表单验证
    添加了新的 ESLint 配置文件，并更新了 package.json 以包含 linting 脚本。通过添加客户端验证和错误处理，增强了创建发票表单中的表单验证。

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -9,30 +9,49 @@ import {redirect} from 'next/navigation'; // 用于导航重定向
 
 // 定义表单数据验证架构，包括ID、客户ID、金额、状态和日期
 const FormSchema = z.object({
-    id: z.string(), // 订单ID
-    customerId: z.string(), // 客户ID
-    amount: z.coerce.number(), // 金额（自动类型转换）
-    status: z.enum(['pending', 'paid']), // 订单状态：待处理或已支付
-    date: z.string(), // 日期
+    id: z.string(),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+        .number()
+        .gt(0, {message: 'Please enter an amount greater than $0.'}),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.',
+    }),
+    date: z.string(),
 });
+
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
 
 // 从 FormSchema 中移除 id 和 date 字段，创建一个新的验证对象
 const CreateInvoice = FormSchema.omit({id: true, date: true});
 
 // 定义异步函数 createInvoice，用于处理表单数据并创建发票
-export async function createInvoice(formData: FormData) {
+export async function createInvoice(prevState: State, formData: FormData) {
     // 解析并验证从表单获取的数据
-    const {customerId, amount, status} = CreateInvoice.parse({
-        customerId: formData.get('customerId'), // 获取客户ID
-        amount: formData.get('amount'), // 获取金额
-        status: formData.get('status'), // 获取订单状态
+    const validatedFields = CreateInvoice.safeParse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
     });
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
 
-    // 将金额转换为美分，以便存储
-    const amountInCents = amount * 100;
-
-    // 获取当前日期，并格式化为 YYYY-MM-DD 格式
-    const date = new Date().toISOString().split('T')[0];
+    const {customerId, amount, status} = validatedFields.data;
+    const amountInCents = amount * 100;    // 将金额转换为美分，以便存储
+    const date = new Date().toISOString().split('T')[0];    // 获取当前日期，并格式化为 YYYY-MM-DD 格式
 
     // 执行 SQL 插入语句，将发票信息保存到数据库中
     try {
@@ -78,8 +97,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
-
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         revalidatePath('/dashboard/invoices');
